b31b5980ea410783d32c33926fd8c4377458f089
vadmark <random223@mail.ru>
2025-10-11 22:57:35 +0300

    Login.

---
+++ internal/handlers/auth.go
@@ -0,0 +1,23 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+)
+
+func Logout() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		session := sessions.Default(c)
+		session.Clear() // удаляем все данные из сессии
+		session.Options(sessions.Options{
+			Path:     "/", // обязательно, иначе cookie не перезапишется
+			MaxAge:   -1,  // удалить cookie сразу
+			HttpOnly: true,
+		})
+		_ = session.Save()
+
+		c.Redirect(http.StatusFound, "/login")
+	}
+}
--- internal/router/router.go
+++ internal/router/router.go
@@ -1,12 +1,16 @@
 package router
 
 import (
+	"fmt"
+	"net/http"
 	"vado_server/internal/appcontext"
 	"vado_server/internal/handlers"
 	"vado_server/internal/repository"
 	"vado_server/internal/services"
 	"vado_server/internal/util"
 
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 )
 
@@ -15,29 +19,57 @@ func SetupRouter(cxt *appcontext.AppContext) *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger(), gin.Recovery())
 	_ = r.SetTrustedProxies(nil)
+	// Статика и шаблоны
 	r.Static("/static", "./internal/static")
 	r.LoadHTMLGlob("internal/templates/*")
 
-	r.GET("/", handlers.ShowIndex)
+	// Настраиваем cookie-сессии
+	store := cookie.NewStore([]byte("super-secret-key"))
+	r.Use(sessions.Sessions("vado-session", store))
 
+	// Сервисы
 	taskRepo := repository.NewTaskRepository(cxt.DB)
 	roleRepo := repository.NewRoleRepository(cxt.DB)
 	taskService := services.NewTaskService(taskRepo)
 	roleService := services.NewRoleService(roleRepo)
-	// HTML-страница
-	r.GET("/tasks", handlers.ShowTasksPage(taskService))
+
+	// Публичные маршруты
+	r.GET("/", handlers.ShowIndex)
+	r.GET("/login", handlers.ShowLoginPage())
+	r.POST("/login", handlers.PerformLogin(cxt))
+
+	r.POST("/logout", handlers.Logout())
+
+	// Защищенные маршруты
+	auth := r.Group("/")
+	auth.Use(AuthRequired())
+	{
+		fmt.Println("Auth")
+		auth.GET("/tasks", handlers.ShowTasksPage(taskService))
+		auth.POST("/tasks", handlers.AddTask(cxt))
+		auth.DELETE("/tasks/:id", handlers.DeleteTask(cxt))
+		auth.GET("/users", handlers.ShowUsers(cxt))
+		auth.POST("/users", handlers.AddUser(cxt))
+		auth.GET("/roles", handlers.ShowRoles(roleService))
+		auth.DELETE("/users/:id", handlers.DeleteUser(cxt))
+	}
+
 	// JSON API
-	r.GET("/api/tasks", handlers.GetTasksJSON(taskService))
-
-	r.POST("/tasks", handlers.AddTask(cxt))
-	r.DELETE("/tasks/:id", handlers.DeleteTask(cxt))
-
-	r.GET("/users", handlers.ShowUsers(cxt))
-	r.POST("/users", handlers.AddUser(cxt))
-
-	r.GET("/roles", handlers.ShowRoles(roleService))
-
-	r.DELETE("/users/:id", handlers.DeleteUser(cxt))
+	//r.GET("/api/tasks", handlers.GetTasksJSON(taskService))
 
 	return r
+}
+
+func AuthRequired() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		session := sessions.Default(c)
+		userID := session.Get("user_id")
+
+		if userID == nil {
+			c.Redirect(http.StatusFound, "/login")
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
 }
