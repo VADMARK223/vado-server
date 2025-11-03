a3d71e0faa595024d218304dc0b6e26a175c9f85
vadmark <random223@mail.ru>
2025-11-04 00:57:26 +0300

    Say hello without token.

--- web/static/js/grpc.ts
+++ web/static/js/grpc.ts
@@ -1,16 +1,35 @@
-import { PingServiceClient } from './pb/PingServiceClientPb';
 import { Empty } from 'google-protobuf/google/protobuf/empty_pb';
+
+import { PingServiceClient } from './pb/PingServiceClientPb';
+import { PingResponse } from './pb/ping_pb'
+
+import { HelloServiceClient } from './pb/HelloServiceClientPb';
+import { HelloRequest, HelloResponse } from './pb/hello_pb';
 
 declare const process: any;
 
 const GRPC_WEB_PORT = process.env.GRPC_WEB_PORT || '1111'
 const GRPC_WEB_URL = 'http://localhost:' + GRPC_WEB_PORT;
-const pingClient = new PingServiceClient(GRPC_WEB_URL, null, null);
 
-export async function pingServer(): Promise<Empty> {
-    return new Promise((resolve, reject) => {
+const pingClient = new PingServiceClient(GRPC_WEB_URL, null, null);
+const helloClient = new HelloServiceClient(GRPC_WEB_URL, null, null);
+
+export async function pingServer(): Promise<PingResponse> {
+    return new Promise<PingResponse>((resolve, reject) => {
         pingClient.ping(new Empty(), {}, (err, resp) => {
-            if (err) reject(err);
+            if (err || !resp) reject(err);
+            else resolve(resp);
+        });
+    });
+}
+
+export async function sayHello(name: string): Promise<HelloResponse> {
+    const req = new HelloRequest();
+    req.setName(name);
+
+    return new Promise<HelloResponse>((resolve, reject) => {
+        helloClient.sayHello(req, {}, (err, resp) => {
+            if (err || !resp) reject(err);
             else resolve(resp);
         });
     });
